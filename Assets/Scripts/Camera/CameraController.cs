9eb0b7bd0f81d9ec5510d5023bdfb4ed71500bdd
Daniel Hansen <da96ha@gmail.com>
2021-06-02 11:26:41 +0200

    Updated CameraController

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -25,7 +25,7 @@ public class CameraController : MonoBehaviour
         {
             currentRoom = currentRoom + 1;
             transform.position = new Vector2(currentRoom * offsetRoom, 0f);
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            //GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
     }
 
